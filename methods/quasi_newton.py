21620eec81af74c200db14793bf50dbc8aa9d6dd
JanaStevanovic28 <jana.faksv2@gmail.com>
2026-09-04 21:22:06 +0200

    updated and added new functions and quasi Newton methods

--- methods/quasi_newton.py
+++ methods/quasi_newton.py
@@ -13,90 +13,269 @@ from operator import add
 class BFGS(LineSearchMethod):
     def __init__(self):
         super().__init__()
-        self.evaluation_numbers = [0, 0, 0]  # val, gradient, hessian
+        self.evaluation_numbers = [0, 0, 0]
 
     def find_minimum(self, eval_function, starting_point, params):
-        """
-        Minimizes a function using the BFGS quasi-Newton method.
-        """
+        epsilon = params["epsilon"]
+        max_iter = params["max_iterations"]
+        work_precision = params["work_precision"]
 
-        epsilon = params["epsilon"]  # get the epsilon
-        max_iter = params["max_iterations"]  # get maximum iterations
-        work_precision = params["work_precision"]  # get work precision
-        eval_function.set_starting_point(starting_point)  # set initial starting point
+        self.evaluation_numbers = [0, 0, 0]
+
+        eval_function.set_starting_point(starting_point)
 
         eye_matrix = np.eye(starting_point.shape[0])
-        inverse_hess_apr = eye_matrix
+        inverse_hess_apr = eye_matrix.copy()
 
-        function_values = []  # List for keeping track of values
-        gradient_values = []  # List for keeping track of gradients
+        function_values = []
+        gradient_values = []
+
         start_time = time.process_time()
-        for iter in range(max_iter):
-            starting_point = eval_function.get_starting_point()
-            val, grad, __ = eval_function.calculate(
-                value=True, gradient=True, hessian=False
-            )
-            self.evaluation_numbers = list(map(add, self.evaluation_numbers, [1, 1, 0]))
 
-            function_values.append(val)
-            gradient_values.append(np.linalg.norm(grad))
+        val, grad, _ = eval_function.calculate(value=True, gradient=True, hessian=False)
+        self.evaluation_numbers = list(map(add, self.evaluation_numbers, [1, 1, 0]))
 
-            # Check for convergence, the gradient not changing
-            if np.linalg.norm(grad) < epsilon:
-                print(f"Converged in {iter} iterations.")
+        function_values.append(val)
+        gradient_values.append(np.linalg.norm(grad))
+
+        iteration = 0
+
+        while iteration < max_iter:
+
+            grad_norm = np.linalg.norm(grad)
+
+            if grad_norm < epsilon:
                 break
 
-            # Compute search direction: p_k = -B_inv * g_k
             direction = -np.matmul(inverse_hess_apr, grad)
 
-            # Use the line search method to calculate the step size. Wolfe is the usual one
             step_size, eval_numbers = self.calculate_step_size(
                 input_function=eval_function,
-                direction=-grad,
+                direction=direction,
                 params=params,
             )
+
             self.evaluation_numbers = list(
                 map(add, self.evaluation_numbers, eval_numbers)
             )
 
-            # Update step
-            next_step = step_size * direction
-            next_val = starting_point + next_step
+            starting_point = eval_function.get_starting_point()
 
-            # Calculate for the next value
-            eval_function.set_starting_point(next_val)
-            _, grad_next, __ = eval_function.calculate(
-                value=False, gradient=True, hessian=False
+            next_step = step_size * direction
+            next_point = starting_point + next_step
+
+            eval_function.set_starting_point(next_point)
+
+            f_prev = val
+
+            f_curr, grad_next, _ = eval_function.calculate(
+                value=True, gradient=True, hessian=False
             )
 
-            if np.sum(np.abs((next_step))) < work_precision:
-                print(
-                    f"Stopped because of a small change in funcion values ({np.sum(np.abs((next_step)))} < {work_precision})"
-                )
-                break
-            # Compute change in gradient: y_k = g_{k+1} - g_k
+            self.evaluation_numbers = list(map(add, self.evaluation_numbers, [1, 1, 0]))
+
+            iteration += 1
+
+            function_values.append(f_curr)
+            gradient_values.append(np.linalg.norm(grad_next))
+
             gradient_delta = grad_next - grad
+            sTy = np.dot(gradient_delta, next_step)
 
-            # BFGS inverse Hessian update formula
-            rho = 1.0 / np.dot(gradient_delta, next_step)
+            if sTy > 1e-12:
+                rho = 1.0 / sTy
 
-            # Use the inverse, Sherman-Morrison formula
-            term1 = eye_matrix - rho * np.outer(next_step, gradient_delta)
-            term2 = eye_matrix - rho * np.outer(gradient_delta, next_step)
-            term3 = rho * np.outer(next_step, next_step)
+                term1 = eye_matrix - rho * np.outer(next_step, gradient_delta)
 
-            inverse_hess_apr = np.dot(term1, np.dot(inverse_hess_apr, term2)) + term3
+                term2 = eye_matrix - rho * np.outer(gradient_delta, next_step)
 
-        # END OF PROCESS
+                term3 = rho * np.outer(next_step, next_step)
+
+                inverse_hess_apr = (
+                    np.dot(term1, np.dot(inverse_hess_apr, term2)) + term3
+                )
+
+            relative_function_change = abs(f_curr - f_prev) / (1 + abs(f_curr))
+
+            val = f_curr
+            grad = grad_next
+
+            if np.linalg.norm(grad) < epsilon:
+                break
+
+            if relative_function_change < work_precision:
+                break
+
         cpu_time = time.process_time() - start_time
 
         return {
             "current_point": eval_function.get_starting_point(),
             "fmin": val,
-            "iteration": iter,
+            "iteration": iteration,
             "function_values": function_values,
             "gradient_values": gradient_values,
-            "grad_norm": gradient_values[iter],
+            "grad_norm": gradient_values[-1],
+            "eval_numbers": self.evaluation_numbers,
+            "exec_time": cpu_time,
+        }
+
+
+class L_BFGS(LineSearchMethod):
+    def __init__(self):
+        super().__init__()
+        self.evaluation_numbers = [0, 0, 0]
+        self.memory = 10
+
+    def find_minimum(self, eval_function, starting_point, params):
+        epsilon = params["epsilon"]
+        max_iter = params["max_iterations"]
+        work_precision = params["work_precision"]
+
+        self.evaluation_numbers = [0, 0, 0]
+
+        eval_function.set_starting_point(starting_point)
+
+        function_values = []
+        gradient_values = []
+
+        s_history = []
+        y_history = []
+        rho_history = []
+
+        start_time = time.process_time()
+
+        val, grad, _ = eval_function.calculate(value=True, gradient=True, hessian=False)
+
+        self.evaluation_numbers = list(map(add, self.evaluation_numbers, [1, 1, 0]))
+
+        function_values.append(val)
+        gradient_values.append(np.linalg.norm(grad))
+
+        iteration = 0
+
+        while iteration < max_iter:
+
+            grad_norm = np.linalg.norm(grad)
+
+            if grad_norm < epsilon:
+                break
+
+            q = grad.copy()
+
+            alpha_values = []
+
+            i = len(s_history) - 1
+
+            while i >= 0:
+                s = s_history[i]
+                y = y_history[i]
+                rho = rho_history[i]
+
+                alpha = rho * np.dot(s, q)
+                alpha_values.append(alpha)
+
+                q = q - alpha * y
+
+                i -= 1
+
+            if len(s_history) > 0:
+                last_s = s_history[-1]
+                last_y = y_history[-1]
+
+                yTy = np.dot(last_y, last_y)
+
+                if yTy > 1e-12:
+                    gamma = np.dot(last_s, last_y) / yTy
+                else:
+                    gamma = 1.0
+            else:
+                gamma = 1.0
+
+            r = gamma * q
+
+            i = 0
+
+            while i < len(s_history):
+                s = s_history[i]
+                y = y_history[i]
+                rho = rho_history[i]
+
+                alpha = alpha_values[len(alpha_values) - 1 - i]
+
+                beta = rho * np.dot(y, r)
+
+                r = r + s * (alpha - beta)
+
+                i += 1
+
+            direction = -r
+
+            step_size, eval_numbers = self.calculate_step_size(
+                input_function=eval_function,
+                direction=direction,
+                params=params,
+            )
+
+            self.evaluation_numbers = list(
+                map(add, self.evaluation_numbers, eval_numbers)
+            )
+
+            starting_point = eval_function.get_starting_point()
+
+            next_step = step_size * direction
+            next_point = starting_point + next_step
+
+            eval_function.set_starting_point(next_point)
+
+            f_prev = val
+
+            f_curr, grad_next, _ = eval_function.calculate(
+                value=True, gradient=True, hessian=False
+            )
+
+            self.evaluation_numbers = list(map(add, self.evaluation_numbers, [1, 1, 0]))
+
+            iteration += 1
+
+            function_values.append(f_curr)
+            gradient_values.append(np.linalg.norm(grad_next))
+
+            gradient_delta = grad_next - grad
+
+            sTy = np.dot(next_step, gradient_delta)
+
+            if sTy > 1e-12:
+                rho = 1.0 / sTy
+
+                s_history.append(next_step.copy())
+                y_history.append(gradient_delta.copy())
+                rho_history.append(rho)
+
+                if len(s_history) > self.memory:
+                    s_history.pop(0)
+                    y_history.pop(0)
+                    rho_history.pop(0)
+
+            relative_function_change = abs(f_curr - f_prev) / (1 + abs(f_curr))
+
+            val = f_curr
+            grad = grad_next
+
+            if np.linalg.norm(grad) < epsilon:
+                break
+
+            if relative_function_change < work_precision:
+                break
+
+        cpu_time = time.process_time() - start_time
+
+        return {
+            "current_point": eval_function.get_starting_point(),
+            "fmin": val,
+            "iteration": iteration,
+            "function_values": function_values,
+            "gradient_values": gradient_values,
+            "grad_norm": gradient_values[-1],
             "eval_numbers": self.evaluation_numbers,
             "exec_time": cpu_time,
         }
@@ -105,90 +284,106 @@ class BFGS(LineSearchMethod):
 class Broyden(LineSearchMethod):
     def __init__(self):
         super().__init__()
-        self.evaluation_numbers = [0, 0, 0]  # val, gradient, hessian
+        self.evaluation_numbers = [0, 0, 0]
 
     def find_minimum(self, eval_function, starting_point, params):
-        """
-        Minimizes a function using the BFGS quasi-Newton method.
-        """
+        epsilon = params["epsilon"]
+        max_iter = params["max_iterations"]
+        work_precision = params["work_precision"]
 
-        epsilon = params["epsilon"]  # get the epsilon
-        max_iter = params["max_iterations"]  # get maximum iterations
-        work_precision = params["work_precision"]  # get work precision
-        eval_function.set_starting_point(starting_point)  # set initial starting point
+        self.evaluation_numbers = [0, 0, 0]
 
-        inverse_hess_apr = np.eye(starting_point.shape[0])
+        eval_function.set_starting_point(starting_point)
 
-        function_values = []  # List for keeping track of values
-        gradient_values = []  # List for keeping track of gradients
+        eye_matrix = np.eye(starting_point.shape[0])
+        inverse_hess_apr = eye_matrix.copy()
+
+        function_values = []
+        gradient_values = []
+
         start_time = time.process_time()
-        for iter in range(max_iter):
-            starting_point = eval_function.get_starting_point()
-            val, grad, __ = eval_function.calculate(
-                value=True, gradient=True, hessian=False
-            )
-            self.evaluation_numbers = list(map(add, self.evaluation_numbers, [1, 1, 0]))
 
-            function_values.append(val)
-            gradient_values.append(np.linalg.norm(grad))
+        val, grad, _ = eval_function.calculate(value=True, gradient=True, hessian=False)
 
-            # Check for convergence, the gradient not changing
-            if np.linalg.norm(grad) < epsilon:
-                print(f"Converged in {iter} iterations.")
+        self.evaluation_numbers = list(map(add, self.evaluation_numbers, [1, 1, 0]))
+
+        function_values.append(val)
+        gradient_values.append(np.linalg.norm(grad))
+
+        iteration = 0
+
+        while iteration < max_iter:
+
+            grad_norm = np.linalg.norm(grad)
+
+            if grad_norm < epsilon:
                 break
 
-            # Compute search direction: p_k = -B_inv * g_k
             direction = -np.matmul(inverse_hess_apr, grad)
 
-            # Use the line search method to calculate the step size. Wolfe is the usual one
             step_size, eval_numbers = self.calculate_step_size(
                 input_function=eval_function,
-                direction=-grad,
+                direction=direction,
                 params=params,
             )
+
             self.evaluation_numbers = list(
                 map(add, self.evaluation_numbers, eval_numbers)
             )
 
-            # Update step
-            next_step = step_size * direction
-            next_val = starting_point + next_step
+            starting_point = eval_function.get_starting_point()
 
-            # Calculate for the next value
-            eval_function.set_starting_point(next_val)
-            _, grad_next, __ = eval_function.calculate(
-                value=False, gradient=True, hessian=False
+            next_step = step_size * direction
+            next_point = starting_point + next_step
+
+            eval_function.set_starting_point(next_point)
+
+            f_prev = val
+
+            f_curr, grad_next, _ = eval_function.calculate(
+                value=True, gradient=True, hessian=False
             )
 
-            if np.sum(np.abs((next_step))) < work_precision:
-                print(
-                    f"Stopped because of a small change in funcion values ({np.sum(np.abs((next_step)))} < {work_precision})"
-                )
-                break
-            # Compute change in gradient: y_k = g_{k+1} - g_k
+            self.evaluation_numbers = list(map(add, self.evaluation_numbers, [1, 1, 0]))
+
+            iteration += 1
+
+            function_values.append(f_curr)
+            gradient_values.append(np.linalg.norm(grad_next))
+
             gradient_delta = grad_next - grad
 
-            # BFGS inverse Hessian update formula
-            # Use the inverse, Sherman-Morrison formula
-            step_mul_term = np.squeeze(
-                np.matmul(next_step[np.newaxis, :], inverse_hess_apr)
-            )
-            denominator = 1.0 / np.dot(step_mul_term, gradient_delta)
-            nominator = np.outer(
-                (next_step - np.matmul(inverse_hess_apr, grad)), step_mul_term
-            )
-            inverse_hess_apr = inverse_hess_apr + denominator * nominator
+            denom = np.dot(next_step, gradient_delta)
 
-        # END OF PROCESS
+            if abs(denom) > 1e-12:
+                inverse_hess_apr = inverse_hess_apr + (
+                    np.outer(
+                        next_step - np.matmul(inverse_hess_apr, gradient_delta),
+                        next_step,
+                    )
+                    / denom
+                )
+
+            relative_function_change = abs(f_curr - f_prev) / (1 + abs(f_curr))
+
+            val = f_curr
+            grad = grad_next
+
+            if np.linalg.norm(grad) < epsilon:
+                break
+
+            if relative_function_change < work_precision:
+                break
+
         cpu_time = time.process_time() - start_time
 
         return {
             "current_point": eval_function.get_starting_point(),
             "fmin": val,
-            "iteration": iter,
+            "iteration": iteration,
             "function_values": function_values,
             "gradient_values": gradient_values,
-            "grad_norm": gradient_values[iter],
+            "grad_norm": gradient_values[-1],
             "eval_numbers": self.evaluation_numbers,
             "exec_time": cpu_time,
         }
